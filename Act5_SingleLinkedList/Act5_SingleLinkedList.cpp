12cc50adf95670c32efe9b145b79db5f3fc48b6f
sekarasriii <sekareeesh@gmail.com>
2024-04-26 11:41:09 +0700

    deklarasi pernyataan previous dimulai

--- Act5_SingleLinkedList/Act5_SingleLinkedList.cpp
+++ Act5_SingleLinkedList/Act5_SingleLinkedList.cpp
@@ -13,5 +13,25 @@ void addNode() {
 	int nim;
 	string nama;
 	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM: ";
+	cin >> nim;
+	cout << "Masukkan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	Node* previous = START;
 
 }
