8e55e08bfde48c5f8fe02c825a30d0547e39c5b6
sekarasriii <sekareeesh@gmail.com>
2024-04-26 11:54:01 +0700

    deklarasi perintah untuk menghapus node dengan tipe data boolean

--- Act5_SingleLinkedList/Act5_SingleLinkedList.cpp
+++ Act5_SingleLinkedList/Act5_SingleLinkedList.cpp
@@ -53,5 +53,25 @@ void addNode() {
 bool searchNode(int nim, Node* current, Node* previous) {
 	previous = START;
 	current = START;
+	while (current != NULL && nim > current->noMhs)
+	{
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+	{
+		return false;
+	}
+	else if (current->noMhs == nim)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+bool deleteNode(int nim) {
 
 }
