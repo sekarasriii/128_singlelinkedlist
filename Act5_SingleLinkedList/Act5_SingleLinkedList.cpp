a731ab30f000d84054e2c100d1b8f40e17cfaa81
sekarasriii <sekareeesh@gmail.com>
2024-04-26 12:21:35 +0700

    menambahkan fungsi switch case

--- Act5_SingleLinkedList/Act5_SingleLinkedList.cpp
+++ Act5_SingleLinkedList/Act5_SingleLinkedList.cpp
@@ -130,5 +130,20 @@ void searchData() {
 
 int main() {
 	int pilihan;
+	do
+	{
+		try {
+			cout << "1. Tambah Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+			cout << "Pilihan: ";
+			cin >> pilihan;
+			switch (pilihan)
+			{
 
+			}
+		}
+	}
 }
